2a990f0e5e2696d2c5ac6c95f597357bae8a629a
kitahoshimgr <mgrhokusei035@gmail.com>
2026-03-15 15:42:17 +0900

    timer fixed

--- src/main.cpp
+++ src/main.cpp
@@ -5,33 +5,32 @@
 #define CAN_RX 15
 #define CAN_TX 16
 
+bool CAM_PWR = true;
+bool CAM_PWR_Prev = true;
+
 hw_timer_t *timer = NULL;
 
 CAN_CREATE CAN(true);
 
-can_setting_t Settings = {
+can_setting_t can_setting = {
   .baudRate = (long)100E3,
   .multiData_send = true,
   .filter_config = CAN_FILTER_DEFAULT,
 };
 
 IRAM_ATTR void counter() {
-  digitalWrite(CAM_CNT, LOW);
-  Serial.println("Camera 3.3V Power off");
+  CAM_PWR = false;
 }
 
 void setup() {
   pinMode(CAM_CNT, OUTPUT);
-  digitalWrite(CAM_CNT, HIGH);
-  timer = timerBegin(0, getApbFrequency() / 1000000, true);
-  timerAttachInterrupt(timer, &counter, false);
-  timerAlarmWrite(timer, 20 * 1000000, false);
+  digitalWrite(CAM_CNT, LOW);
   Serial.begin(115200);
   while (!Serial)
     ;
   delay(1000);
   Serial.println("CAN Sender");
-  if (CAN.begin(Settings, CAN_RX, CAN_TX)) {
+  if (CAN.begin(can_setting, CAN_RX, CAN_TX)) {
     Serial.println("Starting CAN failed!");
   }
   delay(100);
@@ -52,6 +51,9 @@ void setup() {
   default:
     break;
   }
+  timer = timerBegin(0, getApbFrequency() / 1000000, true);
+  timerAttachInterrupt(timer, &counter, false);
+  timerAlarmWrite(timer, 20 * 1000000, false);
 }
 
 void loop() {
@@ -61,12 +63,10 @@ void loop() {
       Serial.println("failed to get CAN data");
     }
     if (Data.id == 0x030) {
-      digitalWrite(CAM_CNT, LOW);
-      Serial.println("Camera 3.3V Power off");
+      CAM_PWR = false;
     }
     if (Data.id == 0x02f) {
-      digitalWrite(CAM_CNT, HIGH);
-      Serial.println("Camera 3.3V Power on");
+      CAM_PWR = true;
     }
     if (Data.id == 0x12a) {
       timerRestart(timer);
@@ -77,16 +77,27 @@ void loop() {
     char cmd = Serial.read();
     Serial.println(cmd);
     if (cmd == 'o') {
-      digitalWrite(CAM_CNT, LOW);
-      Serial.println("Camera 3.3V Power off");
+      CAM_PWR = false;
     }
     if (cmd == 'i') {
-      digitalWrite(CAM_CNT, HIGH);
-      Serial.println("Camera 3.3V Power on");
+      CAM_PWR = true;
     }
     if (cmd == 't') {
       timerRestart(timer);
       timerAlarmEnable(timer);
     }
   }
+  if (CAM_PWR) {
+    digitalWrite(CAM_CNT, HIGH);
+    if (!CAM_PWR_Prev) {
+      Serial.println("Camera 3.3V Power on");
+    }
+  }
+  else {
+    digitalWrite(CAM_CNT, LOW);
+    if (CAM_PWR_Prev) {
+      Serial.println("Camera 3.3V Power off");
+    }
+  }
+  CAM_PWR_Prev = CAM_PWR;
 }
